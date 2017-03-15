d3613cd6826bafe2f855af59446e198bcab7224f
Rune Madsen <rune@runemadsen.com>
2017-03-15 14:05:05 -0400

    edits

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,43 +1,53 @@
- 
-$html = $('<textarea id="codeeditor" width=400px height=400px></textarea>');
-
-$(document).ready(function(){
-
- $('.content').append($html)
-
-
-});
-
-
-
-
-
-var codeeditor = CodeMirror(document.getElementById("codeeditor"), {
-            mode:  "javascript",
-            styleActiveLine: true,
-            lineNumbers: true,
-            lineWrapping: true,
-            autoCloseTags: true,
-            autoCloseBrackets: true,
-            // value: "<!DOCTYPE html><html><head><script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/p5.min.js"></script>",
-             theme : 'solarized',
-            extraKeys: {
+var textarea = document.getElementById("codeeditor")
+var codeeditor = CodeMirror.fromTextArea(textarea, {
+  mode:  "javascript",
+  styleActiveLine: true,
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseTags: true,
+  autoCloseBrackets: true,
+  theme : 'solarized',
+  extraKeys: {
     "Tab": "indentMore"
   }
-            
-        });
+});
 
-// codeeditor.setValue("<!DOCTYPE html><html><head><script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/p5.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/addons/p5.dom.min.js'></script></head><body><script> \n \n function setup(){\n }\n function draw(){ \n } \n </script></body></html>");
-        codeeditor.setValue("function setup(){\n createCanvas(200,200); \n }\n function draw(){ ellipse(40,40,20,20); \n } ");
-        function submit_code()
-        {
-            // codeeditor.save();
-            var cdn = "<script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/p5.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/addons/p5.dom.min.js'></script><script>" 
-            var endcdn="</script></body></html>"
-            var code = cdn + codeeditor.getValue() + endcdn;
-            // console.log(code);
-            var data_url = "data:text/html;charset=utf-8;base64," + $.base64.encode(code);
-            document.getElementById("result").src = data_url;
-        }
-  
+function submit_code()
+{
+    // codeeditor.save();
+    var cdn = "<script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/p5.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/addons/p5.dom.min.js'></script><script>"
+    var endcdn="</script></body></html>"
+    var code = cdn + codeeditor.getValue() + endcdn;
+    // console.log(code);
+    var data_url = "data:text/html;charset=utf-8;base64," + $.base64.encode(code);
+    document.getElementById("result").src = data_url;
+}
 
+// Code to handle versions
+// ---------------------------------------------
+
+var counter = 1;
+
+// Find the save button
+var saveButton = document.getElementById('singlebutton');
+
+// Find the column to put the versions in
+var versionsCol = document.getElementById('versions');
+
+// When that button is clicked
+saveButton.addEventListener('click', function(e) {
+
+  // Don't follow the link
+  e.preventDefault();
+
+  // Create a new div DOM element
+  var version = document.createElement('div')
+  version.className = 'version';
+  version.innerHTML = '<p>Version ' + counter + '</p>';
+
+  // Add it to the #versions column
+  versionsCol.appendChild(version);
+
+  // Increment counter for next version
+  counter++;
+})
